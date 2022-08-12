fca190512d8947287d29941cb15a64308547e308
Andres Bolivar <dre.bolivar@gmail.com>
2022-08-12 15:08:44 -0400

    comment error fixed

--- models/user.js
+++ models/user.js
@@ -2,14 +2,9 @@
 const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       User.hasMany(models.Post, { foreignKey: 'authorId' })
-      User.hasMany(models.Comment), { foreignKey: 'userId' }
+      User.hasMany(models.Comment, { foreignKey: 'userId' })
     }
   }
   User.init(
