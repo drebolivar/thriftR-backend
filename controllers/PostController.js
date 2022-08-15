173acca476e32f03894b19ac54f7f9a170548b19
Alex Campo <abrettc@gmail.com>
2022-08-15 10:28:01 -0500

    added joins for posts and comments calls, as well as get all for all models

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,46 +1,65 @@
-const { Post } = require("../models");
+const { Post, User, Comment } = require('../models')
 
 const getPostsById = async (req, res) => {
   try {
-    const posts = await Post.findByPk(req.params.post_id);
-    res.send(posts);
+    const posts = await Post.findByPk(req.params.post_id)
+    res.send(posts)
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
+
+const getAllPosts = async (req, res) => {
+  try {
+    let posts = await Post.findAll({
+      include: [
+        {
+          model: User
+        },
+        {
+          model: Comment
+        }
+      ]
+    })
+    res.send(posts)
+  } catch (error) {
+    throw error
+  }
+}
 
 const createPost = async (req, res) => {
   try {
-    let postCreate = await Post.create(req.body);
-    res.send(postCreate);
+    let postCreate = await Post.create(req.body)
+    res.send(postCreate)
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 const updatePost = async (req, res) => {
   try {
     let postUpdate = await Post.update(req.body, {
       where: { id: req.params.post_id },
-      returning: true,
-    });
-    res.send(postUpdate);
+      returning: true
+    })
+    res.send(postUpdate)
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 const deletePost = async (req, res) => {
   try {
-    await Post.destroy({ where: { id: req.params.post_id } });
+    await Post.destroy({ where: { id: req.params.post_id } })
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 module.exports = {
   getPostsById,
   createPost,
   updatePost,
   deletePost,
-};
+  getAllPosts
+}
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,10 +1,10 @@
-const Router = require("express").Router();
-const controller = require("../controllers/UserController");
+const Router = require('express').Router()
+const controller = require('../controllers/UserController')
 
 Router.get('/:user_id', controller.getProfileByID)
+Router.get('/', controller.getAllUser)
 Router.post('/', controller.createProfile)
 Router.put('/:user_id', controller.updateProfile)
 Router.delete('/:user_id', controller.deleteProfile)
 
 module.exports = Router
-
