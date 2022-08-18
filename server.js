d887cfcd2edd2b0d21356b9f441bd815183fe26b
Alex Campo <abrettc@gmail.com>
2022-08-18 13:01:37 -0500

    attempt at cors fixes

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const AuthRouter = require('./routes/AuthRouter')
 
 const PORT = process.env.PORT || 3001
 
-app.use(cors())
+const corsOptions = { origin: ['http://localhost:3000'], credentials: true }
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => res.json({ message: 'thriftR Server Live' }))
