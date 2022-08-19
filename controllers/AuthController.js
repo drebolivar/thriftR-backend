167de2af5d2178df9bc18cf06c87947fa4620174
Alex Campo <abrettc@gmail.com>
2022-08-19 10:32:05 -0500

    added comments for clarity and some cors updates

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,11 +2,13 @@ const { User } = require('../models')
 const middleware = require('../middleware')
 
 const Login = async (req, res) => {
+  //Checks if a user exists in the db
   try {
     const user = await User.findOne({
       where: { email: req.body.email },
       raw: true
     })
+    //checks that the passwords match then assigns payload
     if (
       user &&
       (await middleware.comparePassword(user.passwordDigest, req.body.password))
@@ -15,6 +17,7 @@ const Login = async (req, res) => {
         id: user.id,
         email: user.email
       }
+      //creates token and assigns payload
       let token = middleware.createToken(payload)
       return res.send({ user: payload, token })
     }
@@ -25,6 +28,7 @@ const Login = async (req, res) => {
 }
 
 const Register = async (req, res) => {
+  //submits register data and creates the passwordDigest
   try {
     const { email, password, username, profileImg } = req.body
 
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const AuthRouter = require('./routes/AuthRouter')
 
 const PORT = process.env.PORT || 3001
 
+//This sets the permissions for CORS and access to heroku backend. (This is the answer to the CORS blocking access to heroku error)
 const corsOptions = {
   origin: ['http://localhost:3000', 'localhost:3000'],
   methods: ['GET', 'POST', 'DELETE', 'PUT', 'UPDATE'],
@@ -15,6 +16,8 @@ const corsOptions = {
   credentials: true
 }
 
+//corsOptions gets plugged in here in order to work
+//              VV
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
